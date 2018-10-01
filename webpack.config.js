1d511fb33d8019d40faebbc28ea4e6740496f481
mkan0141 <kanzume0141@gmail.com>
2018-10-01 11:53:28 +0900

    :dizzy: Sassの環境を整えた
    これでSassが使えるぞー！！

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,53 @@
 const path = require('path');
+ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    target: 'node',
-    entry: './src/index.tsx',
-    cache: true,
-    mode: 'development', // "production" | "development" | "none"
-    devtool: 'source-map',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+module.exports = [
+    {
+        target: 'node',
+        entry: './src/index.tsx',
+        cache: true,
+        mode: 'development', // "production" | "development" | "none"
+        devtool: 'source-map',
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                test: /\.tsx?$/,
+                use: 'ts-loader'
+            }],
+        },
+        resolve: {
+            extensions: [
+                '.ts',
+                '.tsx',
+                '.js',
+            ]
+        },
     },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            use: 'ts-loader'
-        }],
-    },
-    resolve: {
-        extensions: [
-            '.ts',
-            '.tsx',
-            '.js',
-        ]
-    },
-};
+    {
+        mode: 'development',
+        entry: {
+            style: './src/index.scss'
+        },
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: 'bundle.css'
+        },
+        module: {
+            rules: [
+                {
+                test: /\.scss$/,
+                loader:  ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+                }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin('bundle.css'),
+        ],
+    }
+];
