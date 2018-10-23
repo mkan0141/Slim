1746fc5a0825d9f2187105442645c82679068040
mkan0141 <kanzume0141@gmail.com>
2018-10-23 22:13:05 +0900

    update: 新しくできた ファイル/コンポーネント をimportした

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import MenueButton from './component/MenuButton';
+
 export class App extends React.Component<{}> {
     render(){
         return (
-            <div>
+            <div className='name'>
+                <MenueButton></MenueButton>
             </div>
         );
     }
