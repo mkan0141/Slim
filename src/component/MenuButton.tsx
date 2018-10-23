abb9307299bdf966e37cae953ba7b72d65f69db3
mkan0141 <kanzume0141@gmail.com>
2018-10-23 21:57:41 +0900

    :sparkles: メニューの三本線にアニメーションをつけた．

--- src/component/MenuButton.tsx
+++ src/component/MenuButton.tsx
@@ -1,16 +1,39 @@
 import * as React from 'react';
 
-interface MenueButtonProps{
+interface MenuButtonProps{
 
 }
-export default class MenueButton extends React.Component<{}> {
-    constructor(props: MenueButtonProps){
+
+interface MenuButtonState {
+    is_open: boolean
+}
+
+export default class MenueButton extends React.Component<MenuButtonProps, MenuButtonState> {
+    constructor(props: MenuButtonProps){
         super(props);
+        this.state = {
+            is_open: false,
+        };
+
+        this.menuClick = this.menuClick.bind(this);
     }
+
+    menuClick() {
+        console.log(this.state.is_open);
+        this.setState({
+            is_open: !this.state.is_open,
+        });
+    }
+
+    isOpen(){
+        return ['menu-trigger',
+        this.state.is_open ? '' : 'menu-animation'].join(' ');
+    }
+
     render() {
         return (
             <div className='menu-button'>
-                <a className="menu-trigger" href="#">
+                <a className={this.state.is_open ? 'menu-trigger' : 'menu-trigger menu-animation'} onClick={this.menuClick} href="#">
                     <span></span>
                     <span></span>
                     <span></span>
